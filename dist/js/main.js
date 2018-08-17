1f86fd8b6d02347c95b11b2388d35652d0bf7322
DannyMcMillan <mcmillandanny@gmail.com>
2018-08-17 00:11:17 -0400

    added api

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,13 @@
 "use strict";
 
-console.log("Hello World from app.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+console.log("Vue-Final");
+
+var YOUR_KEY = "91cfde4de28e4fb338abbedd15dc86b5";
+
+axios.get("http://circuslabs.net/proxies/igdb/?user-key=91cfde4de28e4fb338abbedd15dc86b5&endpoint=/games/1942?fields=*").then(function (response) {
+  console.log(response.data);
+}).catch(function (error) {
+  // handle error
+  console.log(error);
+});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
-console.log(`Hello World from app.js! 
-Change this message, and make sure it changes in the browser 
-to verify that you're working in the right files.`)
+console.log(`Vue-Final`);
+
+let YOUR_KEY = "91cfde4de28e4fb338abbedd15dc86b5";
+
+
+
+axios.get("http://circuslabs.net/proxies/igdb/?user-key=91cfde4de28e4fb338abbedd15dc86b5&endpoint=/games/1942?fields=*")
+  .then(function (response) {
+    console.log(response.data);
+})
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+})
