0ffa1c1d9b43dbafbf43f87881953bf621625270
DannyMcMillan <mcmillandanny@gmail.com>
2018-08-21 04:24:16 -0400

    added API calls

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,13 +1,36 @@
-"use strict";
+'use strict';
 
-console.log("Vue-Final");
+console.log('Vue-Final');
 
 var YOUR_KEY = "91cfde4de28e4fb338abbedd15dc86b5";
+var searchField = document.querySelector('.searchField');
+var submitBtn = document.querySelector('button');
 
-axios.get("http://circuslabs.net/proxies/igdb/?user-key=91cfde4de28e4fb338abbedd15dc86b5&endpoint=/games/1942?fields=*").then(function (response) {
-  console.log(response.data);
-}).catch(function (error) {
-  // handle error
-  console.log(error);
+submitBtn.addEventListener('click', function (e) {
+	e.preventDefault();
+	var gameSearch = searchField.value;
+	getGameID(gameSearch);
 });
+
+function getGameID(getID) {
+	axios.get("http://circuslabs.net/proxies/igdb/?user-key=91cfde4de28e4fb338abbedd15dc86b5&endpoint=/games/?search=" + getID + "&fields=id,name").then(function (response) {
+		var gameStuff = getID;
+		getGameFromID(gameStuff);
+	}).catch(function (error) {
+		console.log(error);
+	});
+}
+
+function getGameFromID(id) {
+	axios.get("http://circuslabs.net/proxies/igdb/?user-key=91cfde4de28e4fb338abbedd15dc86b5&endpoint=/games/" + id + "?fields=id,name").then(function (response) {
+		console.log(response.data);
+		console.log(id);
+	}).catch(function (error) {
+		console.log(error);
+	});
+}
+
+// When searched we are getting the ID value from a game, need to figure out how to translate that to an actual title of a game.
+// need a loop in first axios call. need to grab one ID from the ARRAY I AM TIRED
+// if halo is searched it returns 41608 which is the ID number of Halo 1
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,40 @@
 console.log(`Vue-Final`);
 
 let YOUR_KEY = "91cfde4de28e4fb338abbedd15dc86b5";
+let searchField = document.querySelector('.searchField');
+let submitBtn = document.querySelector('button');
 
-
-
-axios.get("http://circuslabs.net/proxies/igdb/?user-key=91cfde4de28e4fb338abbedd15dc86b5&endpoint=/games/1942?fields=*")
-  .then(function (response) {
-    console.log(response.data);
+submitBtn.addEventListener('click', function(e) {
+	e.preventDefault();
+	let gameSearch  = searchField.value;
+	getGameID(gameSearch);
 })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
+
+
+function getGameID(getID) {
+	axios.get("http://circuslabs.net/proxies/igdb/?user-key=91cfde4de28e4fb338abbedd15dc86b5&endpoint=/games/?search=" + getID +"&fields=id,name")
+	  .then(function (response) {
+	  	let gameStuff = getID;
+	  	getGameFromID(gameStuff)
 })
+	  .catch(function (error) {
+	    console.log(error);
+	})
+}
+
+function getGameFromID(id) {
+	axios.get("http://circuslabs.net/proxies/igdb/?user-key=91cfde4de28e4fb338abbedd15dc86b5&endpoint=/games/" + id + "?fields=id,name")
+	  	.then(function (response) {
+	    	console.log(response.data);
+	    		console.log(id);
+		})
+	  	.catch(function (error) {
+	    	console.log(error);
+	})
+}
+
+// When searched we are getting the ID value from a game, need to figure out how to translate that to an actual title of a game.
+// need a loop in first axios call. need to grab one ID from the ARRAY I AM TIRED
+// if halo is searched it returns 41608 which is the ID number of Halo 1
+
+
