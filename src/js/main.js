57e6a97c746b57f0be566df385de916fa6d35574
DannyMcMillan <mcmillandanny@gmail.com>
2018-08-23 14:00:15 -0400

    fixed api calls

--- src/js/main.js
+++ src/js/main.js
@@ -11,27 +11,26 @@ submitBtn.addEventListener('click', function(e) {
 })
 
 
-function getGameID(getID) {
-	axios.get("http://circuslabs.net/proxies/igdb/?user-key=91cfde4de28e4fb338abbedd15dc86b5&endpoint=/games/?search=" + getID +"&fields=id,name")
+function getGameID(gameSearch) {
+	axios.get("http://circuslabs.net/proxies/igdb/?user-key=91cfde4de28e4fb338abbedd15dc86b5&endpoint=/games/?search=" + gameSearch + "&fields=name")
 	  .then(function (response) {
-	  	let gameStuff = getID;
-	  	getGameFromID(gameStuff)
+	  	console.log(response.data)
 })
 	  .catch(function (error) {
 	    console.log(error);
 	})
 }
 
-function getGameFromID(id) {
-	axios.get("http://circuslabs.net/proxies/igdb/?user-key=91cfde4de28e4fb338abbedd15dc86b5&endpoint=/games/" + id + "?fields=id,name")
-	  	.then(function (response) {
-	    	console.log(response.data);
-	    		console.log(id);
-		})
-	  	.catch(function (error) {
-	    	console.log(error);
-	})
-}
+// function getGameFromID(id) {
+// 	axios.get("http://circuslabs.net/proxies/igdb/?user-key=91cfde4de28e4fb338abbedd15dc86b5&endpoint=/games/" + id + "?fields=id,name")
+// 	  	.then(function (response) {
+// 	    	console.log(response.data);
+// 	    		console.log(id);
+// 		})
+// 	  	.catch(function (error) {
+// 	    	console.log(error);
+// 	})
+// }
 
 // When searched we are getting the ID value from a game, need to figure out how to translate that to an actual title of a game.
 // need a loop in first axios call. need to grab one ID from the ARRAY I AM TIRED
